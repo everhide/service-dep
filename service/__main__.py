efcb5a4892d5b5c33232923156643b0d69074912
everhide <i.tolkachnikov@gmail.com>
2022-08-10 14:29:24 +0300

    add commands

--- service/__main__.py
+++ service/__main__.py
@@ -1,7 +1,10 @@
 """Service commands."""
 
+import subprocess
+
 from fire import Fire
 from service import fn
+from pathlib import Path
 from uvicorn import run as run_asgi
 
 
@@ -28,6 +31,30 @@ class Command:
             options['log_config'] = spec.logging.config_file
 
         run_asgi(entrypoint, **options)
+
+    @staticmethod
+    def export_locales():
+        """Export i18n locales config for locale-gen ."""
+        locales = fn.load_locales()
+        locales_path = Path(fn.app_dir() / 'locale.gen').resolve()
+        with open(locales_path, 'w') as locale_gen:
+            for _locale in locales:
+                locale_gen.write(f'{_locale}.UTF-8 UTF-8\n')
+
+    @staticmethod
+    def migrate():
+        """Alembic migrate."""
+        subprocess.call(['python', '-m', 'alembic', 'upgrade', 'head'])
+
+    @staticmethod
+    def make_migration(name: str):
+        """Alembic make migration."""
+        subprocess.call(['python', '-m', 'alembic', 'revision', '-m', 'name'])
+
+    @staticmethod
+    def rollback(rev: str):
+        """Alembic rollback to exact revision."""
+        subprocess.call(['python', '-m', 'alembic', 'downgrade', rev])
 
 
 Fire(Command)
